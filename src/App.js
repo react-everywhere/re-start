417f0a46ddc201df28ab3d8d31598f2f29fa4c4d
Amogh Banta <amogh@Amoghs-MacBook-Pro.local>
2017-04-06 23:59:39 +0530

    chore: code cleanup :wrench:

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Provider} from "react-redux";
 import store from "./utilities/storage/store";
 import TopLevelComponent from './screens/EntryScreen';
