96d155b2024e7505fa13281866591894d333c31c
Jesús Leganés-Combarro <piranna@gmail.com>
2017-10-10 07:03:05 +0200

    Load Reactnative-web components
    Allow to load components that have special components for ReactNative-web under `*.web.js` and `*.web.jsx` extensions. This should be done too on `webpack.config.prod.js` file, or better than that, make this file to inherit and overrride values from there as it's done for example in Django.
    
    If you are busy at this moment, you could be able to add me as a contributor of the project and/or add me as a member of the organization, I have at this moment several clients interested in re-start development (one of them @UnifyMe, whom I'm the CEO and that I think Albert contacted to you recently if you wanted to work with us) and I would be able to dedicate some time to clean and maintain the repo if you want :-)

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -74,7 +74,7 @@ module.exports = {
     // We also include JSX as a common component filename extension to support
     // some tools, although we do not recommend using it, see:
     // https://github.com/facebookincubator/create-react-app/issues/290
-    extensions: ['.js', '.json', '.jsx', ''],
+    extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx', ''],
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
