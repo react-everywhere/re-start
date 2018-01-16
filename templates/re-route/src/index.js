8086f3ed0947e8b0abc89cfbae2aa3b3b01f82e6
Amogh Banta <amoghbanta@gmail.com>
2018-01-16 13:32:31 +0530

    remove use of App.js

--- templates/re-route/src/index.js
+++ templates/re-route/src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactNative from 'react-native'
-import App from './App'
+import EntryScreen from './screens/EntryScreen';
+import Routing, {Router} from './utilities/routing/index';
+
+const Route = Routing.Route;
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Route path='/' component={EntryScreen}/>
+            </Router>
+        );
+    }
+}
 
 ReactNative.render(<App/>, document.getElementById('root'));
