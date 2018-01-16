59d2cdf3d34afd709cd7d2975a312ff29f33b704
Amogh Banta <amoghbanta@gmail.com>
2018-01-16 16:34:52 +0530

    update dependencies

--- templates/re-dux/src/utilities/routing/index.android.js
+++
@@ -1,3 +0,0 @@
-import * as Routing from 'react-router-native';
-export default Routing;
-export const Router = Routing.NativeRouter;
--- templates/re-dux/src/utilities/routing/index.js
+++
@@ -1,3 +0,0 @@
-import * as Routing from 'react-router-dom';
-export default Routing;
-export const Router = Routing.BrowserRouter;
