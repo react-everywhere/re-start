979b3f93eb0c079d801590be1c976731a613e1a8
Amogh Banta <amoghbanta@gmail.com>
2018-01-16 13:30:33 +0530

    remove App.js :fire:

--- templates/re-route/src/actions/ActionTypes.js
+++
@@ -1,4 +0,0 @@
-// This file serves as a common location for all your action constants
-
-export const SOMETHING_HAPPENED_SUCCESSFULLY = "SOMETHING_HAPPENED_SUCCESSFULLY";
-export const SOMETHING_FAILED = "SOMETHING_FAILED";
