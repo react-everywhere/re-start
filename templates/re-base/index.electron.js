1ad36301ebfa32f2a90d8130a3411a63e538ecf5
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-04-14 18:22:37 +0200

    Use default size for Electron window (800x600)

--- templates/re-base/index.electron.js
+++ templates/re-base/index.electron.js
@@ -6,7 +6,7 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 1000, height: 800});
+    mainWindow = new BrowserWindow();
 
     // and load the index.html of the app.
     mainWindow.loadURL(`file://${__dirname}/build/index.html`);
